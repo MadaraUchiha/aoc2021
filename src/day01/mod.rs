6ff76661f4cc3f7cfe8576f8a990bf5ba8a4f544
Madara Uchiha <me@madara.ninja>
2021-12-01 17:27:07 +0200

    day01

---
+++ src/day01/mod.rs
@@ -0,0 +1,30 @@
+pub fn part1(input: String) -> i32 {
+  let parsed_input: Vec<i32> = parse_input(input);
+  return parsed_input
+    .windows(2)
+    .filter(|window| window[1] > window[0])
+    .count()
+    .try_into()
+    .unwrap();
+}
+
+pub fn part2(input: String) -> i32 {
+  let parsed_input: Vec<i32> = parse_input(input);
+
+  return parsed_input
+    .windows(3)
+    .map(|window| window.iter().sum::<i32>())
+    .collect::<Vec<i32>>()
+    .windows(2)
+    .filter(|window| window[1] > window[0])
+    .count()
+    .try_into()
+    .unwrap();
+}
+
+fn parse_input(input: String) -> Vec<i32> {
+  return input
+    .split_whitespace()
+    .map(|n| n.parse().expect("Not a number"))
+    .collect();
+}
